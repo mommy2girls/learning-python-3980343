215091920449d96b6bad188d5bfc9d18d584bd86
mommy2girls <lindsay.johnson@santeecooper.com>
2025-09-17 13:36:39 +0000

    training

--- Start/Ch2/dict_start.py
+++ Start/Ch2/dict_start.py
@@ -2,22 +2,34 @@
 # Example file for complex types
 
 
-# Dictionary: a key-value data structure
+# Dictionary: a key-value data structure, keys must be unique
+mydict = {
+  "one" : 1,
+  "two" : 2,
+  3 : "three",
+  4.5 : ["four", "point", "five"]
+}
 
+# print(mydict)
 
 # dictionaries are accessed via keys
-
+# print(mydict["one"])
+# print(mydict[3])
 
 # you can also set dictionary data by creating a new key
-
-
+mydict["seven"] = 7
+print(mydict)
 # Trying to access a nonexistent key will produce an error
-
+# print(mydict["blarg"]) KeyError: 'blarg'
 
 # To avoid this, you can use the "in" operator to see if a key exists
-
+# print("two" in mydict)  True
+# print("blarg" in mydict)  False
 
 # You can retrieve all of the keys and values from a dictionary
-
+# print(mydict.keys()) -> dict_keys(['one', 'two', 3, 4.5, 'seven'])
+# print(mydict.values()) -> dict_values([1, 2, 'three', ['four', 'point', 'five'], 7])
 
 # You can also iterate over all the items in a dictionary
+for key, val in mydict.items():
+  print(key, val)
